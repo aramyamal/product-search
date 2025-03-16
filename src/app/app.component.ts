1e82aa67071683419de7d78ed45094e027397ccb
aramyamal <aram@aramjamal.com>
2025-03-16 22:48:41 +0100

    refactor: split app into preliminary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { ResultsComponent } from './components/results/results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchComponent, ResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/results/results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ItemCardComponent } from '../item-card/item-card.component';
+
+@Component({
+  selector: 'app-results',
+  imports: [ItemCardComponent],
+  templateUrl: './results.component.html',
+  styleUrl: './results.component.css'
+})
+export class ResultsComponent {
+
+}
